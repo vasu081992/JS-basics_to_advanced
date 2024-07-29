c6a28d060b79ec57183c6885d399e7a93efd491d
vasuve_altair <vasuve@altair.com>
2024-07-29 10:23:57 +0530

    generator fn js

---
+++ Tutorial26-Generatorfunction/script.js
@@ -0,0 +1,36 @@
+function* gen() {
+  let ask1 = yield "2 + 2 = ?";
+
+  alert(ask1); // 4
+
+  let ask2 = yield "3 * 3 = ?"
+
+  alert(ask2); // 9
+}
+
+let generator = gen();
+
+alert( generator.next().value ); // "2 + 2 = ?"
+
+alert( generator.next(4).value ); // "3 * 3 = ?"
+
+alert( generator.next(9).done ); // true
+
+
+
+
+// generator.return
+// generator.return(value) finishes the generator execution and return the given value.
+
+
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const g = gen();
+
+g.next();        // { value: 1, done: false }
+g.return('foo'); // { value: "foo", done: true }
+g.next();        // { value: undefined, done: true }
